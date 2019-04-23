4cf8a54988f89255f990d5cd061e069cd65135cc
Marcus Schwemer <marcus.schwemer@in2code.de>
2019-04-23 09:29:16 +0200

    [FEATURE] Style backend login

--- packages/training/ext_tables.php
+++ packages/training/ext_tables.php
@@ -1,2 +1,7 @@
 <?php
 defined('TYPO3_MODE') || die();
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] = '#E50043';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginLogo'] = 'EXT:in2template/Resources/Public/Images/Backend/logo-notch-th-owl_300.svg';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] = 'EXT:in2template/Resources/Public/Images/Backend/logo-th-owl.svg';
+$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginBackgroundImage'] = 'EXT:in2template/Resources/Public/Images/Backend/university_building.png';
